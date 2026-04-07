b4f0ce2dc68d69c90de75ac2d8a44eee2346b4a9
0423_Trần Trung Dũng <dungtrantrung603@gmail.com>
2026-04-07 19:37:45 +0700

    feat(enrollments): add unique index for active registration pairs

---
+++ schemas/enrollments.js
@@ -0,0 +1,24 @@
+let mongoose = require('mongoose');
+let enrollmentSchema = mongoose.Schema({
+    student: {
+        type: mongoose.Types.ObjectId,
+        ref: 'student',
+        required: true
+    },
+    courseClass: {
+        type: mongoose.Types.ObjectId,
+        ref: 'courseclass',
+        required: true
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    timestamps: true
+})
+enrollmentSchema.index(
+    { student: 1, courseClass: 1, isDeleted: 1 },
+    { unique: true, partialFilterExpression: { isDeleted: false } }
+)
+module.exports = new mongoose.model('enrollment', enrollmentSchema)
